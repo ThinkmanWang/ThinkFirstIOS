31d3746c66aa8bf1df4c3216381306223f602ee8
Thinkman Wang <wangxf1985@gmail.com>
2020-12-22 18:57:54 +0800

    Initial Commit

---
+++ ThinkFirstIOS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ThinkFirstIOS
+//
+//  Created by 王晓丰 on 2020/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
